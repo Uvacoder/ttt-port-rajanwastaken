98c256453f0f706686e372fb73718a5b847e8127
Uvacoder <uva22940coder@gmail.com>
2022-10-27 08:03:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['file.rajanagarwal.xyz', 'file.heyrajan.com', 'emojipedia-us.s3.dualstack.us-west-1.amazonaws.com', 'cloud-r2onbrszd-hack-club-bot.vercel.app', 'images.unsplash.com', 'cloud-cssrzjtwc-hack-club-bot.vercel.app', 'cloud-ra5alevdj-hack-club-bot.vercel.app', 'cloud-16c6pal7a-hack-club-bot.vercel.app', 'user-images.githubusercontent.com', 'cloud-e9m8zg3xl-hack-club-bot.vercel.app'],
+    domains: ['file.rajanagarwal.xyz', 'file.heyrajan.com', 'emojipedia-us.s3.dualstack.us-west-1.amazonaws.com', 'cloud-r2onbrszd-hack-club-bot.vercel.app', 'images.unsplash.com', 'cloud-cssrzjtwc-hack-club-bot.vercel.app', 'cloud-ra5alevdj-hack-club-bot.vercel.app', 'cloud-16c6pal7a-hack-club-bot.vercel.app', 'user-images.githubusercontent.com', 'raw.githubusercontent.com', 'cloud-e9m8zg3xl-hack-club-bot.vercel.app'],
   },
 }
 
